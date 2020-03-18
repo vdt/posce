948c026eb494d120c878f1edb7c887e2306623a6
Stephen Malone <61864999+posce@users.noreply.github.com>
2020-03-18 17:34:32 +1000

    Added 'posce.tools' package.

--- posce/__init__.py
+++ posce/__init__.py
@@ -5,3 +5,5 @@ Package definition for 'posce'.
 VERSION_DATE   = '2020-03-18'
 VERSION_NUMBER = '0.0.0'
 VERSION_STRING = f'Posce version {VERSION_NUMBER} ({VERSION_DATE}).'
+
+from posce import tools
