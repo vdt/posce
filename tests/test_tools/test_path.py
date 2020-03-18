2aa3403dfbb783cde78f671a401481b0171811be
Stephen Malone <61864999+posce@users.noreply.github.com>
2020-03-18 18:56:57 +1000

    Changed 'posce.tools.path' test variables.

--- tests/test_tools/test_path.py
+++ tests/test_tools/test_path.py
@@ -11,27 +11,27 @@ def s(path):
 
 def test_base():
     # success
-    assert path.base('/dir/file.ext') == 'file.ext'
+    assert path.base('/dir/file.txt') == 'file.txt'
 
 def test_clean():
     # success
-    assert path.clean('/././file.ext') == s('/file.ext')
+    assert path.clean('/././file.txt') == s('/file.txt')
 
 def test_expand():
     # setup
     os.environ.update({'HOME': '/home', 'TEST': 'test'})
 
     # success
-    assert path.expand('$HOME/$TEST/file.ext') == '/home/test/file.ext'
+    assert path.expand('$HOME/$TEST/file.txt') == '/home/test/file.txt'
 
 def test_ext():
     # success
-    assert path.ext('/dir/file.ext') == 'ext'
+    assert path.ext('/dir/file.txt') == 'txt'
 
 def test_join():
     # success
-    assert path.join('foo', 'bar', 'file.ext') == s('foo/bar/file.ext')
+    assert path.join('foo', 'bar', 'file.txt') == s('foo/bar/file.txt')
 
 def test_name():
     # success
-    assert path.name('/dir/file.ext') == 'file'
+    assert path.name('/dir/file.txt') == 'file'
